96aca8c09f1a940547e2d2d0215ce02904cff0b3
faridnfl <faridnaufal34@gmail.com>
2024-03-30 10:18:29 +0800

    add all files

---
+++ components/header/header.vue
@@ -0,0 +1,9 @@
+<style>
+@import url("/assets/css/header.css");
+</style>
+
+<template>
+    <div>
+     <h1>Welcome to the homepage</h1>
+    </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<script setup>
+import Header from "../components/header/header.vue";
+</script>
+
+<style>
+@import url("/assets/css/global.css");
+</style>
+
+<template>
+  <div>
+    <Header/>
+  </div>
+</template>
+  
